d709e81a23cd421c71e875efd25fc892ed744c0a
Griggs <peter@deepai.org>
2021-01-06 10:01:39 -0800

    mobile ios browser css clean-up

--- src/components/map-chart/map-chart.js
+++ src/components/map-chart/map-chart.js
@@ -98,6 +98,7 @@ export const MapChart = ({
               strokeLinecap="round"
               strokeLinejoin="round"
               transform="scale(0.5) translate(-12, -24)"
+              style={{ pointerEvents: "none" }}
             >
               <circle cx="12" cy="10" r="3" />
               <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
